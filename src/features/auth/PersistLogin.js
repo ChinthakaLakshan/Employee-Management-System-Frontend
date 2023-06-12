6949bf388f7ba61f0ff420536ecf4e4e560dfe97
ChinthakaLakshan <88089519+ChinthakaLakshan@users.noreply.github.com>
2023-06-12 15:35:58 +0530

    persist

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -1,7 +1,7 @@
 import { Outlet, Link } from "react-router-dom"
 import { useEffect, useRef, useState } from 'react'
 import { useRefreshMutation } from "./authApiSlice"
-import usePersist from "../../hooks/usePersist"
+import usePersist from "../../hooks/userPersist"
 import { useSelector } from 'react-redux'
 import { selectCurrentToken } from "./authSlice"
 
