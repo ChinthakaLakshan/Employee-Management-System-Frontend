0405204f28b49dc161c2b8169113499906919d3d
ChinthakaLakshan <88089519+ChinthakaLakshan@users.noreply.github.com>
2023-07-29 23:49:55 +0530

    7/29

---
+++ src/features/leave/NewLeave.js
@@ -0,0 +1,23 @@
+import React from "react";
+import NewLeaveForm from './NewLeaveForm';
+import { useGetUsersQuery } from '../users/usersApiSlice';
+import PulseLoader from 'react-spinners/PulseLoader';
+import useTitle from '../../hooks/useTitle';
+
+const NewLeave = () => {
+    useTitle(' New Leave Request');
+
+    const { users } = useGetUsersQuery("usersList", {
+        selectFromResult: ({ data }) => ({
+            users: data?.ids.map(id => data?.entities[id])
+        }),
+    });
+
+    if (!users?.length) return <PulseLoader color={"#FFF"} />
+
+    const content = <NewLeaveForm users={users} />;
+
+    return content;
+};
+
+export default NewLeave;
